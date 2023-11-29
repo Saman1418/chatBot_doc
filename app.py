45fdba3770f699ad930513eb906ef85bceb953b5
saman <92148332+saman18Tudo@users.noreply.github.com>
2023-11-29 15:05:13 +0530

    changes

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ import os
 
 # os.environ["OPENAI_API_KEY"] = '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'
 # os.environ["OPENAI_API_KEY"] = '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'
-os.environ["OPENAI_API_KEY"] = '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'
+# os.environ["OPENAI_API_KEY"] = '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'
+os.environ["OPENAI_API_KEY"] = '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'
 
 def construct_index(directory_path):
     max_input_size = 4096
